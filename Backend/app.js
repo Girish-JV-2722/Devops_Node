9867491e3bb1dae0837462d78c0069532d4e156b
MandarUmare <90469953+MandarUmare@users.noreply.github.com>
2024-07-11 15:38:49 +0530

    gitAuthentication done

--- Backend/app.js
+++ Backend/app.js
@@ -4,11 +4,19 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+const githubRouter = require('./middleware/github-auth');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const passport = require('passport')
+const cors = require('cors'); // Import the cors package
+const session = require('express-session');
 
 var app = express();
+
+// Enable CORS for specific origins
+app.use(cors({
+  origin: '*'
+}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,8 +28,27 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: true,
+    secret: process.env.SESSION_SECRET,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function (user, cb) {
+  cb(null, user);
+});
+passport.deserializeUser(function (obj, cb) {
+  cb(null, obj);
+});
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/auth/github', githubRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

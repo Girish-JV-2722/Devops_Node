c5a2f9dc528c662b7aa29fccaea0a844c4cd458d
awaze7 <awazeshaikh7@gmail.com>
2024-07-22 23:06:17 +0530

    updated the deployments table as per the requirements

--- Frontend/src/components/DeploymentsPage.jsx
+++ Frontend/src/components/DeploymentsPage.jsx
@@ -16,11 +16,52 @@ function DeplymentsPage() {
       try {
         // const response = await axios.get('http://localhost:3000/project/getAllProjects');
         // setProjects(response.data);
-        setProjects([
-          { projectId: "11", projectName: "Project K", status: "successfully deployed", url: "http://example.com/project-k" },
-          { projectId: "222", projectName: "Project 2", status: "failed to deploy", url: "http://example.com/project-2" },
-          { projectId: "3", projectName: "Project 3", status: "yet to configure", url: "http://example.com/project-3" },
-        ]);
+        // setProjects([
+        //   { projectId: "11", projectName: "Project K", status: "successfully deployed", url: "http://example.com/project-k" },
+        //   { projectId: "222", projectName: "Project 2", status: "failed to deploy", url: "http://example.com/project-2" },
+        //   { projectId: "3", projectName: "Project 3", status: "yet to configure", url: "http://example.com/project-3" },
+        // ]);
+        setProjects(
+          [
+            {
+              "newDeployment": {
+                "deploymentId": 76, "userId": 90469953, "applicationId": 94, "status": true, "log": "Something", "environment": "EC2",
+                "createdAt": "2024-07-22T15:06:01.070Z", "updatedAt": "2024-07-22T15:06:01.071Z"
+              },
+              "deploydata": {
+                "status": true, "publicIp": null, "port": 3000
+              }
+            },
+            {
+              "newDeployment": {
+                "deploymentId": 77, "userId": 90469954, "applicationId": 95, "status": false, "log": "Error", "environment": "Lambda",
+                "createdAt": "2024-07-23T16:07:02.072Z", "updatedAt": "2024-07-23T16:07:02.073Z"
+              },
+              "deploydata": {
+                "status": false, "publicIp": "192.168.1.1", "port": 8080
+              }
+            },
+            {
+              "newDeployment": {
+                "deploymentId": 78, "userId": 90469955, "applicationId": 96, "status": true, "log": "Success", "environment": "ECS",
+                "createdAt": "2024-07-24T17:08:03.074Z", "updatedAt": "2024-07-24T17:08:03.075Z"
+              },
+              "deploydata": {
+                "status": true, "publicIp": "192.168.1.2", "port": 9090
+              }
+            },
+            {
+              "newDeployment": {
+                "deploymentId": 79, "userId": 90469956, "applicationId": 97, "status": false, "log": "Failed to start", "environment": "EC2",
+                "createdAt": "2024-07-25T18:09:04.076Z", "updatedAt": "2024-07-25T18:09:04.077Z"
+              },
+              "deploydata": {
+                "status": false, "publicIp": "192.168.1.3", "port": 7070
+              }
+            },
+          ]
+        )
+
         setLoading(false);
       } catch (err) {
         setError(err.message);
@@ -29,6 +70,11 @@ function DeplymentsPage() {
     };
     fetchProjects();
   }, []);
+
+  useEffect(() => {
+    console.log(projects);
+  }, [projects]);
+
 
   const handleTerminateInstance = async (projectId) => {
     // try {
@@ -52,9 +98,9 @@ function DeplymentsPage() {
 
       // Mock response
       const mockResponse = {
-          data: {
-              success: true, // Change to false to simulate a failed termination
-          }
+        data: {
+          success: true, // Change to false to simulate a failed termination
+        }
       };
 
       // Use the mock response instead of the actual API call
@@ -62,22 +108,20 @@ function DeplymentsPage() {
       const response = mockResponse;
 
       if (response.data.success) {
-          toast("Instance terminated successfully");
-          // Optionally, update the project status to reflect the termination
-          setProjects((prevProjects) => prevProjects.map(project => 
-              project.projectId === projectId ? { ...project, status: 'terminated' } : project
-          ));
+        toast("Instance terminated successfully");
+       // Remove the project from the table after successful termination
+      setProjects((prevProjects) => prevProjects.filter(project => project.newDeployment.deploymentId !== projectId));
       } else {
-          toast("Failed to terminate instance");
+        toast("Failed to terminate instance");
       }
     } catch (error) {
-        toast("Error terminating instance");
+      toast("Error terminating instance");
     }
   };
 
-  const handleConfigure = (projectId) => {
-    navigate(`/configure/${projectId}`);
-  };
+  // const handleConfigure = (projectId) => {
+  //   navigate(`/configure/${projectId}`);
+  // };
 
   const handleAccessLink = (url) => {
     window.open(url, '_blank');
@@ -86,40 +130,28 @@ function DeplymentsPage() {
   const columns = React.useMemo(
     () => [
       {
-        Header: 'Project Name',
-        accessor: 'projectName',
+        // replace with name when backend team gives ProjectName in response
+        Header: 'Project ID',
+        accessor: 'newDeployment.deploymentId',
         className: 'text-gray-900 font-medium',
       },
       {
-        Header: 'Actions',
-        accessor: 'projectId',
-        Cell: ({ value }) => (
-          <button
-            onClick={() => handleConfigure(value)}
-            className="bg-green-500 text-white py-1 px-2 rounded-lg shadow hover:bg-green-600 transition duration-300"
-          >
-            Configure
-          </button>
-        ),
-        className: 'text-center',
-      },
-      {
         Header: 'Status',
-        accessor: 'status',
+        accessor: 'newDeployment.status',
         Cell: ({ value }) => (
-          <span className={`flex items-center justify-center w-36 text-white h-8 py-1 px-2 rounded-lg ${value === 'successfully deployed' ? 'bg-green-600' : value === 'failed to deploy' ? 'bg-red-600' : 'bg-gray-600'}`}>
-            {value}
+          <span className={`flex items-center justify-center w-36 text-white h-8 py-1 px-2 rounded-lg ${value ? 'bg-green-600' : 'bg-red-600'}`}>
+            {value ? 'successfully deployed' : 'failed to deploy'}
           </span>
         ),
         className: 'text-center',
       },
       {
         Header: 'Access',
-        accessor: 'url',
+        accessor: 'deploydata.publicIp',
         Cell: ({ row }) => (
-          row.original.status === 'successfully deployed' ? (
+          row.original.newDeployment.status ? (
             <button
-              onClick={() => handleAccessLink(row.original.url)}
+              onClick={() => handleAccessLink(`http://${row.original.deploydata.publicIp}:${row.original.deploydata.port}`)}
               className="bg-blue-500 text-white py-1 px-2 rounded-lg shadow hover:bg-blue-600 transition duration-300"
             >
               Link
@@ -132,17 +164,17 @@ function DeplymentsPage() {
         Header: 'Terminate EC2 Instance',
         accessor: 'terminate',
         Cell: ({ row }) => (
-            row.original.status === 'successfully deployed' ? (
-                <button
-                    onClick={() => handleTerminateInstance(row.original.projectId)}
-                    className="bg-red-500 text-white py-1 px-2 rounded-lg shadow hover:bg-red-600 transition duration-300"
-                >
-                    Terminate
-                </button>
-            ) : <span className='items-center justify-center py-1 px-5'>-</span>
+          row.original.newDeployment.status ? (
+            <button
+              onClick={() => handleTerminateInstance(row.original.newDeployment.deploymentId)}
+              className="bg-red-500 text-white py-1 px-2 rounded-lg shadow hover:bg-red-600 transition duration-300"
+            >
+              Terminate
+            </button>
+          ) : <span className='items-center justify-center py-1 px-5'>-</span>
         ),
         className: 'text-center',
-    },
+      },
     ],
     []
   );
@@ -173,11 +205,12 @@ function DeplymentsPage() {
             <table {...getTableProps()} className="w-full divide-y divide-gray-200">
               <thead className="bg-gray-50">
                 {headerGroups.map((headerGroup) => (
-                  <tr {...headerGroup.getHeaderGroupProps()}>
+                  <tr {...headerGroup.getHeaderGroupProps()} key={headerGroup.id}>
                     {headerGroup.headers.map((column) => (
                       <th
                         {...column.getHeaderProps()}
                         className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                        key={column.id}
                       >
                         {column.render('Header')}
                       </th>
@@ -189,11 +222,12 @@ function DeplymentsPage() {
                 {rows.map((row) => {
                   prepareRow(row);
                   return (
-                    <tr {...row.getRowProps()}>
-                      {row.cells.map((cell) => (
+                    <tr {...row.getRowProps()} key={row.id}>
+                      {row.cells.map((cell, index) => (
                         <td
                           {...cell.getCellProps()}
                           className="px-6 py-4 whitespace-nowrap text-sm text-gray-900"
+                          key={`${row.id}-${cell.column.id}-${index}`} // Unique key for each cell
                         >
                           {cell.render('Cell')}
                         </td>
@@ -202,6 +236,8 @@ function DeplymentsPage() {
                   );
                 })}
               </tbody>
+
+
             </table>
           </div>
         )}
@@ -212,3 +248,6 @@ function DeplymentsPage() {
 }
 
 export default DeplymentsPage;
+
+
+
